596d0bcf0784a1f5e50bbd8f90b0869de03c6651
gussan <lawson0721@gmail.com>
2023-09-29 13:20:02 +0900

    メモ機能、メモ削除搭載

--- working_history/app.py
+++ working_history/app.py
@@ -1,11 +1,8 @@
 from flask_sqlalchemy import SQLAlchemy
-from flask import Flask, render_template, request, redirect, flash, url_for
+from flask import Flask, render_template, request, redirect, flash, url_for, jsonify
 import os
-from datetime import datetime
-from flask_migrate import Migrate
-from flask import jsonify
 from datetime import datetime, timedelta
-
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -19,7 +16,7 @@ class Employee(db.Model):
     name = db.Column(db.String(80), nullable=False)
     category = db.Column(db.String(80), nullable=False)
 
-class EmployeeDetail(db.Model): 
+class EmployeeDetail(db.Model):
     __tablename__ = 'employee_detail'
     id = db.Column(db.Integer, primary_key=True)
     project_name = db.Column(db.String(80), nullable=False)
@@ -27,26 +24,22 @@ class EmployeeDetail(db.Model):
     period = db.Column(db.String(80), nullable=False)
     price = db.Column(db.Float, nullable=True)
     employee_id = db.Column(db.Integer, db.ForeignKey('employee.id'), nullable=False)
-    employee = db.relationship('Employee', backref='employee_details')  # これを追加
+    employee = db.relationship('Employee', backref='employee_details')
     start_date = db.Column(db.Date, nullable=True)
     end_date = db.Column(db.Date, nullable=True)
     memo = db.Column(db.String(255), nullable=True)
+    memo_month = db.Column(db.String(7), nullable=True)
 
-
-#社員の登録
 @app.route('/add_employee', methods=['POST'])
 def add_employee():
     name = request.form.get('employee_name')
     category = request.form.get('category')
-    
     if not name or not category:
         flash('名前とカテゴリーは必須です', 'error')
         return redirect(url_for('index'))
-
     new_employee = Employee(name=name, category=category)
     db.session.add(new_employee)
     db.session.commit()
-
     flash(f'{name}を追加しました', 'success')
     return redirect(url_for('index'))
 
@@ -59,29 +52,20 @@ def index(category=None):
         employees = Employee.query.all()
     return render_template('index.html', employees=employees, current_category=category)
 
-
-#社員の削除
 @app.route('/delete_employee/<int:id>', methods=['POST'])
 def delete_employee(id):
     employee = Employee.query.get_or_404(id)
-    
-    # この社員に関連するEmployeeDetailも削除
     for detail in employee.employee_details:
         db.session.delete(detail)
-
     db.session.delete(employee)
     db.session.commit()
-    
     flash(f'{employee.name}を削除しました', 'success')
     return redirect(url_for('index'))
 
-
-# 社員の編集について
 @app.route('/employee_detail/<int:id>', methods=['GET', 'POST'])
 def employee_detail(id):
     employee = Employee.query.get_or_404(id)
     detail = EmployeeDetail.query.filter_by(employee_id=id).first()
-
     if request.method == 'POST':
         name = request.form.get('employee_name')
         category = request.form.get('category')
@@ -94,15 +78,11 @@ def employee_detail(id):
         end_date = datetime.strptime(end_date_str, '%Y-%m-%d').date() if end_date_str else None
         period = f"{start_date_str} から {end_date_str}"
         memo = request.form.get('memo')
-
-
         if not name or not category:
             flash('名前とカテゴリーは必須です', 'error')
             return render_template('employee_detail.html', employee=employee, detail=detail)
-
         employee.name = name
         employee.category = category
-
         if detail:
             detail.project_name = project_name
             detail.overview = overview
@@ -112,57 +92,74 @@ def employee_detail(id):
             detail.end_date = end_date
             detail.memo = memo
         else:
-            new_detail = EmployeeDetail(project_name=project_name, overview=overview, start_date=start_date,end_date=end_date,period=period, price=float(price), employee_id=employee.id,memo=memo)
+            new_detail = EmployeeDetail(
+                project_name=project_name, 
+                overview=overview, 
+                start_date=start_date,
+                end_date=end_date,
+                period=period, 
+                price=float(price), 
+                employee_id=employee.id,
+                memo=memo
+            )
             db.session.add(new_detail)
-
         db.session.commit()
-
         flash(f'{name}の情報を更新しました', 'success')
         return redirect(url_for('index'))
-
     return render_template('employee_detail.html', employee=employee, detail=detail)
+
+@app.route('/update_memo', methods=['POST'])
+def update_memo():
+    employee_detail_id = request.json.get('employee_detail_id')
+    memo_month = request.json.get('memo_month')
+    memo = request.json.get('memo')
+    detail = EmployeeDetail.query.filter_by(id=employee_detail_id, memo_month=memo_month).first()
+    if not detail:
+        new_detail = EmployeeDetail(
+            employee_id=employee_detail_id, 
+            memo_month=memo_month, 
+            memo=memo,
+            project_name='プロジェクト名未設定',
+            period='未定'
+        )
+        db.session.add(new_detail)
+    else:
+        detail.memo = memo
+    db.session.commit()
+    return jsonify({'status': 'success'})
+
+@app.route('/delete_memo', methods=['POST'])
+def delete_memo():
+    employee_detail_id = request.json.get('employee_detail_id')
+    memo_month = request.json.get('memo_month')
+
+    detail = EmployeeDetail.query.filter_by(employee_id=employee_detail_id, memo_month=memo_month).first()
+    if detail:
+        db.session.delete(detail)
+        db.session.commit()
+        return jsonify({'status': 'success'})
+    else:
+        return jsonify({'status': 'error', 'message': '該当するメモが見つかりませんでした。'})
+
+
 
 @app.route('/api/employee_periods')
 def api_employee_periods():
-    try:
-        # 現在の月を取得
-        current_month = datetime.today().month
-        current_year = datetime.today().year
-        # 今後6ヶ月間の期間内のイベントをフィルタリングするための終了日を計算します
-        end_month = current_month + 12
-        end_year = current_year
-        if end_month > 12:
-            end_month -= 12
-            end_year += 1
+    details = EmployeeDetail.query.all()
+    colors = ['#3498DB', '#E74C3C', '#2ECC71', '#F39C12', '#9B59B6']
 
-        # 今の月から6ヶ月間のdetailsを取得
-
-        # details = EmployeeDetail.query.filter(
-        #     EmployeeDetail.start_date >= datetime(current_year, current_month, 1),
-        #     EmployeeDetail.start_date < datetime(end_year, end_month, 1)
-        # ).all()
-        
-        details = EmployeeDetail.query.all()
-   
-        colors = ['#3498DB', '#E74C3C', '#2ECC71', '#F39C12', '#9B59B6']  # ここで使いたい色を指定する
-
-        events = [{
-            'title': detail.employee.name,
-            'start': detail.start_date.strftime('%Y-%m-%d'),
-            'end': (detail.end_date + timedelta(days=1)).strftime('%Y-%m-%d'),
-            'color': colors[detail.employee_id % len(colors)],
-            'employeeId': detail.employee_id,
-            'memo': detail.memo
-        } for detail in details]
-        
-        return jsonify(events)
-    except Exception as e:
-        app.logger.error(f"Error occurred: {str(e)}")
-        return jsonify({"error": str(e)}), 500
-
-# データベース初期化の処理
-with app.app_context():
-    db.create_all()
+    events = [{
+        'title': detail.employee.name,
+        'start': detail.start_date.strftime('%Y-%m-%d') if detail.start_date else None,
+        'end': (detail.end_date + timedelta(days=1)).strftime('%Y-%m-%d') if detail.end_date else None,
+        'color': colors[detail.employee_id % len(colors)],
+        'employeeId': detail.employee_id,
+        'memo': detail.memo,
+        'memo_month': detail.memo_month,
+        'project_start_month': detail.start_date.strftime('%Y-%m') if detail.start_date else None,
+        'project_end_month': detail.end_date.strftime('%Y-%m') if detail.end_date else None
+    } for detail in details]
+    return jsonify(events)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- working_history/static/main.js
+++ working_history/static/main.js
@@ -5,73 +5,139 @@ function formatDate(dateString) {
     return yyyy + '-' + mm;
 }
 
-function daysInMonth(year, month) {
-    return new Date(year, month, 0).getDate();
-}
-
 document.addEventListener("DOMContentLoaded", function () {
     const currentDate = new Date();
     const currentYear = currentDate.getFullYear();
     const currentMonth = currentDate.getMonth();
     const memoButtons = document.querySelectorAll('.memo-btn');
-    memoButtons.forEach(btn => {
-        btn.addEventListener('click', function() {
-            const memoText = prompt("メモを入力してください：", "");
-            if (memoText) {
-                btn.setAttribute('data-memo', memoText);
-            }
-        });
-    });
+    let events = [];
 
     fetch('/api/employee_periods')
         .then(response => response.json())
         .then(data => {
-            const events = data.map(event => {
+            events = data.map(event => {
                 return {
                     ...event,
                     start: formatDate(event.start),
                     end: formatDate(event.end),
-                    title: '',
-                    backgroundColor: event.color
+                    backgroundColor: event.color,
+                    memo_month: event.memo_month
                 };
             });
+
+            function isBetween(target, start, end) {
+                return target >= start && target <= end;
+            }
+
+            memoButtons.forEach(btn => {
+                btn.addEventListener('click', function(e) {
+                    e.stopPropagation();
+                    
+                    const employeeId = btn.closest('.employee-calendar').getAttribute('data-employee-id');
+                    const monthTitle = btn.parentElement.querySelector('.month-title').textContent.trim();
+                    const year = monthTitle.split("年")[0];
+                    const month = monthTitle.split("年")[1].split("月")[0];
+                    const memoMonthFormat = `${year}-${month.padStart(2, '0')}`;
+                
+                    let existingMemo = events.find(event => event.employeeId == employeeId && event.memo_month == memoMonthFormat);
+                    let memoText = existingMemo ? existingMemo.memo : "";
+                    const inputText = "メモを入力してください。メモを消す場合、テキストをすべて削除してOKボタンを押してください:";
+                
+                    const newMemo = prompt(inputText, memoText);
+                
+                    if (newMemo && newMemo !== memoText) {
+                        // メモの更新
+                        const postData = {
+                            'employee_detail_id': parseInt(employeeId),
+                            'memo_month': memoMonthFormat,
+                            'memo': newMemo
+                        };
+                
+                        fetch('/update_memo', {
+                            method: 'POST',
+                            headers: {
+                                'Content-Type': 'application/json'
+                            },
+                            body: JSON.stringify(postData)
+                        })
+                        .then(response => response.json())
+                        .then(data => {
+                            if (data.status !== 'success') {
+                                alert('メモの保存に失敗しました。');
+                            }
+                        });
+                    } else if (!newMemo && existingMemo) {
+                        // メモの削除
+                        const postData = {
+                            'employee_detail_id': parseInt(employeeId),
+                            'memo_month': memoMonthFormat
+                        };
+                
+                        fetch('/delete_memo', {
+                            method: 'POST',
+                            headers: {
+                                'Content-Type': 'application/json'
+                            },
+                            body: JSON.stringify(postData)
+                        })
+                        .then(response => response.json())
+                        .then(data => {
+                            if (data.status !== 'success') {
+                                alert('メモの削除に失敗しました。');
+                            }
+                        });
+                    }
+                });
+            });
+            
+            
 
             document.querySelectorAll('.employee-calendar').forEach((calendarContainer) => {
                 const employeeId = parseInt(calendarContainer.dataset.employeeId);
                 const employeeEvents = events.filter(event => event.employeeId == employeeId);
 
                 for (let i = 0; i < 12; i++) {
-                    const targetYear = (currentMonth + i) >= 12 ? currentYear + 1 : currentYear;
                     const targetMonth = (currentMonth + i) % 12;
-                    const displayStartDate = new Date(targetYear, targetMonth, 1);
-                    const displayEndDate = new Date(targetYear, targetMonth + 1, 0);
-
-                    const currentMonthEvents = employeeEvents.filter(event =>
-                        new Date(event.start) <= displayEndDate && new Date(event.end) >= displayStartDate
-                    );
-
+                    const targetYear = (currentMonth + i) >= 12 ? currentYear + 1 : currentYear;
                     const calendarEl = calendarContainer.querySelector('.month-calendar:nth-child(' + (i + 1) + ')');
 
-                    if (calendarEl) {
-                        const monthTitleEl = calendarEl.querySelector('.month-title');
-                        const displayMonth = displayStartDate.getMonth() + 1;
-                        const displayYear = displayStartDate.getFullYear();
-                        monthTitleEl.textContent = `${displayYear}年\n${displayMonth}月`;
+                    const monthTitleEl = calendarEl.querySelector('.month-title');
+                    const displayMonth = targetMonth + 1;
+                    monthTitleEl.textContent = `${targetYear}年${displayMonth}月`;
 
+                    const memoMonthFormat = `${targetYear}-${displayMonth.toString().padStart(2, '0')}`;
+                    const currentMonthEvents = employeeEvents.filter(event => event.memo_month == memoMonthFormat);
 
-                        if (currentMonthEvents.length > 0) {
-                            calendarEl.style.backgroundColor = currentMonthEvents[0].backgroundColor;
+                    // 開始月、終了月の色を変更する
+                    const withinProjectDuration = employeeEvents.some(event => 
+                        isBetween(memoMonthFormat, event.start, event.end)
+                    );
+
+                    if (withinProjectDuration) {
+                        calendarEl.style.backgroundColor = employeeEvents[0].backgroundColor;
+                    }
+
+                    if (currentMonthEvents.length > 0) {
+                        const memoBtn = calendarEl.querySelector('.memo-btn');
+                        if (memoBtn) {
+                            memoBtn.style.color = "red";
                         }
+                    }
 
-                        calendarEl.addEventListener('click', function () {
-                            let relatedEvents = events.filter(event => event.employeeId == employeeId);
-                            relatedEvents.forEach(event => {
-                                alert(`スタッフID: ${event.employeeId}, 開始月: ${event.start}, 終了月: ${event.end}, メモ: ${event.memo || 'なし'}`);
-                            });
+                    calendarEl.addEventListener('click', function() {
+                        let relatedEvents = events.filter(event => event.employeeId == employeeId && event.memo_month == memoMonthFormat);
+                
+                        relatedEvents.forEach(event => {
+                            let startMonth = event.start ? event.start.split('-').slice(0,2).join('-') : '未定';
+                            let endMonth = event.end ? event.end.split('-').slice(0,2).join('-') : '未定';
+                            
+                            alert(`スタッフ名: ${event.title}, メモ: ${event.memo || 'なし'}`);//開始月: ${startMonth}, 終了月: ${endMonth},//
                         });
                         
-                    }
+                        
+                    });
                 }
             });
         });
 });
+
